a2e658c11434b66763635b03f9a921e819fb9189
nannandeesh <nandeeshkv11@gmail.com>
2024-12-05 05:24:31 +0530

    added tmp

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 # Define the directory where the Excel files will be stored
-directory = "/mnt/persistent/excel_files"
+directory = "/tmp/excel_files"
 if not os.path.exists(directory):
     os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
 
